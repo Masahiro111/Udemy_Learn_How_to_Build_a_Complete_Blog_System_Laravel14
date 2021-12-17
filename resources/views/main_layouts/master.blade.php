f05195598af57baa83744bcbae1c3a7cb5425e19
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-12-17 23:17:13 +0900

    finished part 6

--- resources/views/main_layouts/master.blade.php
+++ resources/views/main_layouts/master.blade.php
@@ -91,6 +91,20 @@
 
                                     @auth
                                     <li class="btn-cta"><a href="{{ route('login') }}"><span>Logout</span></a></li>
+
+                                    <li class="has-dropdown">
+                                        <a href="#">{{ auth()->user()->name }} <span class="caret"></span></a>
+                                        <ul class="dropdown">
+                                            <li><a
+                                                   onclick="event.preventDefault(); document.getElementById('nav-logout-form').submit()"
+                                                   href="#">Logout</a>
+
+                                                <form id="nav-logout-form" action="{{ route('logout')}}" method="post">
+                                                    @csrf
+                                                </form>
+                                            </li>
+                                        </ul>
+                                    </li>
                                     @endauth
                                 </ul>
                             </div>
