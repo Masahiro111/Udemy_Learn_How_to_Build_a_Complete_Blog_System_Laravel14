18b55ff9597511c96c0cb2cd464df1f7bdfa0513
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-12-18 20:48:57 +0900

    finished part 9

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('posts')->insert([
+            'title' => 'This is title.',
+            'slug' => 'This is slug.',
+            'excerpt' => 'This is excerpt.',
+            'body' => 'This is body.',
+            'user_id' => 1,
+        ]);
+    }
+}
