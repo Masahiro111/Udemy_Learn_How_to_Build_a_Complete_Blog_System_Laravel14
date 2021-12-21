8d14bd90f194ff0b12b19c8f1ff5b72f8010c928
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-12-21 19:47:54 +0900

    finished part 15

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,9 @@ class DatabaseSeeder extends Seeder
         ]);
 
         \App\Models\Role::factory(1)->create();
+        \App\Models\Category::factory(50)->create();
         \App\Models\User::factory(10)->create();
+
+        \App\Models\Post::factory(50)->create();
     }
 }
