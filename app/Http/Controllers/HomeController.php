7bc7a94c2d3868e0239030565d450c6241acae1a
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-12-23 18:05:46 +0900

    finished part 21

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,11 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $posts = Post::withCount('comments')->get();
+        $posts = Post::latest()->withCount('comments')->paginate(10);
+
+        // return view('home', [
+        //     'posts' => $posts
+        // ]);
 
         return view('home', compact('posts'));
     }
