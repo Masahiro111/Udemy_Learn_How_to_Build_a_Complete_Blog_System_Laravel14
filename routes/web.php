47450123d7c1366895e7a830b96751565ef7cb52
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-12-29 18:02:07 +0900

    finished part 28

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AboutController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostsController;
 use App\Models\Category;
@@ -77,9 +78,7 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/posts/{post:slug}', [PostsController::class, 'show'])->name('posts.show');
 Route::post('/posts/{post:slug}', [PostsController::class, 'addComment'])->name('posts.add_comment');
 
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
+Route::get('/about', AboutController::class)->name('about');
 
 Route::get('/contact', function () {
     return view('contact');
