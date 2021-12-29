5aa25d9e52b1a9e4314ddd88c616dcbdda27011c
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-12-29 17:27:37 +0900

    finished part 27

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -33,6 +33,6 @@ class PostsController extends Controller
         $attributes['user_id'] = auth()->id();
         $comment = $post->comments()->create($attributes);
 
-        return redirect('/posts/' . $post->slug . '#comment_' . $comment->id)->with('success' . 'Comment has been added.');
+        return redirect('/posts/' . $post->slug . '#comment_' . $comment->id)->with('success', 'Comment has been added.');
     }
 }
